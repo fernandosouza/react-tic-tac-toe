99eaba44cea7ec708500c730beec577b1bfe2e3e
Fernando Souza <nandosouzafilho@gmail.com>
2017-04-21 13:05:24 -0300

    Removes unnecessary tests after adding React Router

--- src/setup/Setup.test.js
+++ src/setup/Setup.test.js
@@ -3,22 +3,6 @@ import Setup from './Setup';
 import { shallow } from 'enzyme';
 
 describe('Game', () => {
-  it('should inform players` name to the parent component', () => {
-    const onFinishSetupMock = jest.fn();
-    const wrapper = shallow(<Setup onFinishSetup={onFinishSetupMock} />);
-    const playerOneInput = wrapper.find('#playerOne');
-    const playerTwoInput = wrapper.find('#playerTwo');
-    
-    playerOneInput.value = 'Fernando';
-    playerTwoInput.value = 'Souza';
-    playerOneInput.simulate('change', {target: {value: playerOneInput.value}});
-    playerTwoInput.simulate('change', {target: {value: playerTwoInput.value}});
-
-    wrapper.find('form').simulate('submit', new Event('submit'));
-    expect(onFinishSetupMock.mock.calls[0][0]).toBe('Fernando');
-    expect(onFinishSetupMock.mock.calls[0][1]).toBe('Souza');
-  });
-
   it('should not call onFinishSetup if the first player`s name is missing', () => {
     const onFinishSetupMock = jest.fn();
     const wrapper = shallow(<Setup onFinishSetup={onFinishSetupMock} />);
