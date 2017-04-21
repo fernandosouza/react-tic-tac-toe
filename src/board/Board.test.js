f48d5a16e6eb7650e95ffd8623614e1f59997714
Fernando Souza <nandosouzafilho@gmail.com>
2017-04-21 13:25:02 -0300

    Increasing Board class coverage

--- src/board/Board.test.js
+++ src/board/Board.test.js
@@ -22,4 +22,20 @@ describe('Board', () => {
     expect(wrapper.find('.grid').at(1).hasClass('filled')).toBe(false);
     expect(wrapper.find('.grid').at(2).hasClass('filled')).toBe(true);
   });
+
+  it('should call a callback function after clicking on slots', () => {
+    const callbackMock = jest.fn();
+    const wrapper = shallow(<Board onSlotClick={callbackMock} />);
+
+    wrapper.find('.grid').at(0).simulate('click');
+    expect(callbackMock.mock.calls.length).toBe(1);
+  });
+
+  it('should call a callback function with the slot index as argument after clicking on slots', () => {
+    const callbackMock = jest.fn();
+    const wrapper = shallow(<Board onSlotClick={callbackMock} />);
+
+    wrapper.find('.grid').at(1).simulate('click');
+    expect(callbackMock.mock.calls[0][0]).toBe(1);
+  });
 });
