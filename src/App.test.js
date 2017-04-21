83283a7ccd506e656e37e6ccf8b54e545a508a6c
Fernando Souza <nandosouzafilho@gmail.com>
2017-04-21 13:12:21 -0300

    Mocking router params

--- src/App.test.js
+++ src/App.test.js
@@ -5,5 +5,10 @@ import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  let params = {
+    firstPlayer: 'Fernando',
+    secondPlayer: 'Souza'
+  };
+
+  ReactDOM.render(<App match={ {params} } />, div);
 });
