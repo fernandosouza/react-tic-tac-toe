9f51cf3b3469574d6d9aac163430d00b85d2f839
Fernando Souza <nandosouzafilho@gmail.com>
2017-04-21 00:10:17 -0300

    Uses a more descriptive variable name for mocks

--- src/setup/Setup.test.js
+++ src/setup/Setup.test.js
@@ -4,8 +4,8 @@ import { shallow } from 'enzyme';
 
 describe('Game', () => {
   it('should inform players` name to the parent component', () => {
-    const myMock = jest.fn();
-    const wrapper = shallow(<Setup onFinishSetup={myMock} />);
+    const onFinishSetupMock = jest.fn();
+    const wrapper = shallow(<Setup onFinishSetup={onFinishSetupMock} />);
     const playerOneInput = wrapper.find('#playerOne');
     const playerTwoInput = wrapper.find('#playerTwo');
     
@@ -15,31 +15,31 @@ describe('Game', () => {
     playerTwoInput.simulate('change', {target: {value: playerTwoInput.value}});
 
     wrapper.find('form').simulate('submit', new Event('submit'));
-    expect(myMock.mock.calls[0][0]).toBe('Fernando');
-    expect(myMock.mock.calls[0][1]).toBe('Souza');
+    expect(onFinishSetupMock.mock.calls[0][0]).toBe('Fernando');
+    expect(onFinishSetupMock.mock.calls[0][1]).toBe('Souza');
   });
 
   it('should not call onFinishSetup if the first player`s name is missing', () => {
-    const myMock = jest.fn();
-    const wrapper = shallow(<Setup onFinishSetup={myMock} />);
+    const onFinishSetupMock = jest.fn();
+    const wrapper = shallow(<Setup onFinishSetup={onFinishSetupMock} />);
     const playerTwoInput = wrapper.find('#playerTwo');
     
     playerTwoInput.value = 'Souza';
     playerTwoInput.simulate('change', {target: {value: playerTwoInput.value}});
 
     wrapper.find('form').simulate('submit', new Event('submit'));
-    expect(myMock.mock.calls.length).toBe(0);
+    expect(onFinishSetupMock.mock.calls.length).toBe(0);
   });
 
   it('should not call onFinishSetup if the second player`s name is missing', () => {
-    const myMock = jest.fn();
-    const wrapper = shallow(<Setup onFinishSetup={myMock} />);
+    const onFinishSetupMock = jest.fn();
+    const wrapper = shallow(<Setup onFinishSetup={onFinishSetupMock} />);
     const playerOneInput = wrapper.find('#playerTwo');
     
     playerOneInput.value = 'Souza';
     playerOneInput.simulate('change', {target: {value: playerOneInput.value}});
 
     wrapper.find('form').simulate('submit', new Event('submit'));
-    expect(myMock.mock.calls.length).toBe(0);
+    expect(onFinishSetupMock.mock.calls.length).toBe(0);
   });
 });
