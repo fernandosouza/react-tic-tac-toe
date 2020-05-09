688993fce1fdaee6b055cc85773247a194a349f9
Fernando Souza <nandosouzafilho@gmail.com>
2020-05-09 11:30:46 +0200

    Winner animation improvemetns

--- src/board/Slot.js
+++ src/board/Slot.js
@@ -1,48 +1,56 @@
 import React from 'react';
-import styled, {css} from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+import { ReactComponent as X } from './x.svg';
+import { ReactComponent as Circle } from './circle.svg';
+
+const scale = keyframes`
+  0% {
+    transform: scale(1);
+  }
+
+  100% {
+    transform: scale(1.1);
+  }
+`;
+
+const Player1 = styled(X)`
+  width: 100px;
+  height: 100px;
+
+  ${props => props.winner && css`
+    animation: ${scale} infinite alternate ease-in-out .54s;
+  `}
+`
+
+const Player2 = styled(Circle)`
+  width: 100px;
+  height: 100px;
+
+  ${props => props.winner && css`
+    animation: ${scale} infinite alternate ease-in-out .54s;
+  `}
+`
 
 const SlotWrapper = styled.div`
   --dimensions: calc(var(--board-size) / 3);
 
+  /*TODO: How to move this block to a shared place? */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
   background-color: var(--base-color);
-  background-repeat: no-repeat;
-  background-size: 40%;
-  background-position: center;
   border: var(--grid-border);
   height: var(--dimensions);
   width: var(--dimensions);
+  transition: background-color 150ms linear;
   cursor: pointer;
-
-  -webkit-transition: background-color 150ms linear;
-        -ms-transition: background-color 150ms linear;
-            transition: background-color 150ms linear;
-
-  @keyframes win {
-    0% {
-      background-size: 40%;
-    }
-
-    100% {
-      background-size: 50%;
-    }
-  }
 
   ${props => !props.player && css`
     :hover {
       background-color: #3e5368;
     }
-  `}
-
-  ${props => props.player === 1 && css`
-    background-image: url('${process.env.PUBLIC_URL}/x.svg');
-  `}
-
-  ${props => props.player === 2 && css`
-    background-image: url('${process.env.PUBLIC_URL}/circle.svg');
-  `}
-
-  ${props => props.winner && css`
-    animation: win infinite alternate ease-in-out .54s;
   `}
 `;
 
@@ -50,9 +58,14 @@ export const Slot = props => {
   return (
     <SlotWrapper
       data-testid={`Slot${props.index}`}
-      player={props.player}
-      winner={props.winner}
       onClick={() => props.onSlotClick(props.index)}
-    />
+    >
+      {
+        {
+          1: <Player1 winner={props.winner} />,
+          2: <Player2 winner={props.winner} />
+        }[props.player]
+      }
+    </SlotWrapper>
   );
 };
