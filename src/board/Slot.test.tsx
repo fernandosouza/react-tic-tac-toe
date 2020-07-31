b5a332a3718b3fd901dcce4f2d364dec2c748f0b
Fernando Souza <nandosouzafilho@gmail.com>
2020-07-31 22:36:24 +0200

    Adds better support to styled-components and snapshot testing

--- src/board/Slot.test.tsx
+++ src/board/Slot.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Slot } from './Slot';
 import { create } from 'react-test-renderer';
+import 'jest-styled-components';
 
 describe('Slot', () => {
   it('renders slot', () => {
